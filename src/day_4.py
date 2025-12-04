1dee7637244a3335a970a2f5829e45c3ee83236a
chhk <chorhatarahuduketuri@gmail.com>
2025-12-04 09:59:23 +0000

    Day 4 Star 1

---
+++ src/day_4.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from src.settings import PUZZLE_INPUT_PATH
+
+surrounding_directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+
+def is_accessible(grid: np.ndarray, position: str, i: int, j: int) -> int:
+    """Important assumption: grid is definitely square, so len(grid) == len(grid[0])"""
+    if position == ".":
+        return 0  # False: No paper to be accessed, regardless of surrounding positions
+    counter = 0
+    for d_i, d_j in surrounding_directions:
+        s_i = d_i + i
+        s_j = d_j + j
+        if s_i < 0 or s_i >= len(grid) or s_j < 0 or s_j >= len(grid):  # if off the side of the grid
+            counter += 1
+        elif grid[s_i, s_j] == ".":  # else if on the grid
+            counter += 1
+    if counter > 4:
+        return 1  # True
+    return 0  # False
+
+
+def compute_star_1(puzzle_input: str) -> int:
+    grid = np.asarray([[position for position in row.strip()] for row in puzzle_input.strip().split()], str)
+    accessibility = np.asarray(
+        [[is_accessible(grid, position, i, j) for j, position in enumerate(row)] for i, row in enumerate(grid)]
+    )
+    return accessibility.sum()
+
+
+def compute_star_2(puzzle_input: str) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print("day 4, star 1: ", compute_star_1(open(PUZZLE_INPUT_PATH / "day4.txt").read()))
+    print("day 4, star 2: ", compute_star_2(open(PUZZLE_INPUT_PATH / "day4.txt").read()))
---
+++ test/test_day_4.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src import day_4
+
+input_1_day_4 = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+input_2_day_4 = """"""
+
+output_day_4_star_1 = 13
+output_day_4_star_2 = ...
+
+day_4_star_1_test_input = [(input_1_day_4, output_day_4_star_1)]
+day_4_star_2_test_input = [(input_2_day_4, output_day_4_star_2)]
+
+
+@pytest.mark.parametrize("puzzle_input, output", day_4_star_1_test_input)
+def test_day_4_star_1(puzzle_input: str, output: int):
+    assert day_4.compute_star_1(puzzle_input) == output
+
+
+@pytest.mark.parametrize("puzzle_input, output", day_4_star_2_test_input)
+def test_day_4_star_2(puzzle_input: str, output: int):
+    assert day_4.compute_star_2(puzzle_input) == output
